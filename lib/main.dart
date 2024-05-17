a5fd3fea922087832b29505b88f99bfc4e2d81bb
Ezequiel Brilhante <ezequielrb7@gmail.com>
2024-05-17 10:40:49 -0300

    criando o componente Questao

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,52 @@
 import 'package:flutter/material.dart';
 
+import './questao.dart';
+
 main() {
   runApp(PerguntaApp());
 }
 
-class PerguntaApp extends StatelessWidget {
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada++;
+    });
+    print(_perguntaSelecionada);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
+
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
-              title: Text('Perguntas'),
-              backgroundColor: Color.fromRGBO(69, 71, 238, 0.992),
+              title: const Text(
+                'Perguntas',
+              ),
+              backgroundColor: const Color.fromRGBO(69, 71, 238, 0.992),
             ),
-            body: Text('Olá Flutter ')));
+            body: Column(
+              children: <Widget>[
+                Questao(perguntas[_perguntaSelecionada]),
+                ElevatedButton(
+                    onPressed: _responder, child: Text('Resposta 1')),
+                ElevatedButton(
+                    onPressed: _responder, child: Text('Resposta 2')),
+                ElevatedButton(onPressed: _responder, child: Text('Resposta 3'))
+              ],
+            )));
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  @override
+  createState() {
+    return _PerguntaAppState();
   }
 }
